cfa5048b577ee04a4e6fb9c7f3e49b5b5a6625d2
RyanMSC <montsryan@gmail.com>
2017-09-03 18:32:55 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,71 @@
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) throws Exception 
+	{	
+	    Scanner scanner = new Scanner(System.in);
+	    String ecep, x, aux;    
+        int low = 0;
+        int high = 699307 - 1; //numero total de registros no dat é 699307
+        int mid, achou;
+	    
+		RandomAccessFile f = new RandomAccessFile("cep_ordenado.dat", "r");
+		
+		Endereco e = new Endereco();
+		Endereco t = new Endereco();
+		
+		
+		//699307
+		
+	    System.out.println("Digite o CEP que está procurando:");
+	    ecep=scanner.nextLine();
+		x = ecep;
+		
+		System.out.println("Você está procurando o cep: " + x);
+		
+		System.out.println("-----Percorrendo a lista através de busca binária---------");
+		//Busca Binária cortando de metade em metade
+		while (low <= high) {
+			
+            mid = (low + high) / 2;
+            
+            f.seek(mid*300);
+            //System.out.println("Ponto do arquivo no txt: " + f.getFilePointer());	
+            
+    		t.leEndereco(f);
+    		System.out.println("CEP da linha achado nessa iteração: " + t.getCep());
+    		
+    		aux = t.getCep();
+
+            if (aux.compareTo(x) < 0) {
+                low = mid + 1;
+            } else if (aux.compareTo(x) > 0) {
+                high = mid - 1;
+            } else {
+                //return mid;
+            	achou = mid;
+            	//System.out.println("CEP da linha achado nessa iteração: " + t.getCep());
+            	System.out.println("----------------------Achou!-----------------------------");
+                break;
+            }
+            
+            
+        }
+		
+		
+		System.out.println("Logradouro: " + t.getLogradouro());
+		System.out.println("Rua: " + t.getBairro());
+		System.out.println("Cidade: " + t.getCidade());
+		System.out.println("Estado: " + t.getEstado());
+		System.out.println("Sigla: " + t.getSigla());
+		System.out.println("CEP: " + t.getCep());
+		f.close();
+	}
+	
+
+}
+
+
